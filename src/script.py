abd6cefa0bc7868acb1192aab15c9a43ce183ca5
porsche <83725666+nilotpolas@users.noreply.github.com>
2024-12-31 14:33:23 +0530

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2024 AVS LAB, IIT Guwahati
+# All rights reserved.
+# 
+# Authors:
+# Chandan Karfa, Nilotpola Sarma
+# 
+# This software is licensed under the terms of the MIT License.
+# See the LICENSE file for details.
+#
+# For queries or feedback, please contact:
+# Nilotpola Sarma (s.nilotpola@iitg.ac.in)
+# 
+# This code is provided "as is" without warranty of any kind, express or implied,
+# including but not limited to the warranties of merchantability, fitness for a particular
+# purpose, and noninfringement. In no event shall the authors or copyright holders be
+# liable for any claim, damages, or other liability, whether in an action of contract,
+# tort, or otherwise, arising from, out of, or in connection with the software or the use
+# or other dealings in the software.
 import subprocess
 import sys
 import json
